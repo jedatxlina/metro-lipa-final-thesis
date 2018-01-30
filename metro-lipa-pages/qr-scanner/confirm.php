1cd63f2d7692dd928a2d2779794ad79779215d3d
jedatxlina <jmatthewatx.lina@gmail.com>
2018-01-31 00:41:54 +0800

    DELETED

--- metro-lipa-pages/qr-scanner/confirm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$id = $_GET['result'];
-
-echo "$id";
-?>
--- metro-lipa-pages/try.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-$dte  = '1/31/2018';
-$dt   = new DateTime();
-$date = $dt->createFromFormat('m/d/Y', $dte);
-echo $date->format('Y-m-d');
-?>
