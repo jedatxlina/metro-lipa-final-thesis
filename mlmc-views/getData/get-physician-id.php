a5320db8fbbd616a640eccdb2df7d3fcbd28a70c
francisvgi <francisvonnilagan@gmail.com>
2018-02-19 17:27:06 +0800

    Page redirect upon user login to newly registered users

---
+++ mlmc-views/getData/get-physician-id.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'connection.php';
+$accid = $_GET['accid'];
+$sel = mysqli_query($con,"SELECT * FROM physicians WHERE AccountID='$accid'");
+
+$data = array();
+
+while ($row = mysqli_fetch_array($sel)) {
+    $data[] = array(
+        "PhysicianID"=>$row['PhysicianID'],
+        "LastName"=>$row['LastName'],
+        "FirstName"=>$row['FirstName'],
+        "MiddleName"=>$row['Address'],
+        "Birthdate"=>$row['Birthdate'],
+        "Specialization"=>$row['Specialization'],
+        "ProfessionalFee"=>$row['ProfessionalFee'],
+        "Email"=>$row['Email']);
+}
+
+echo json_encode($data);
+?>
+
+									
--- mlmc-views/insertData/insert-user.php
+++ mlmc-views/insertData/insert-user.php
@@ -7,6 +7,12 @@ $accesstype = $_GET['accesstype'];
 $password = $_GET['password'];
 $email = $_GET['email'];
 
+if ($accesstype==4)
+{
+    $query2= "INSERT into physicians(PhysicianID,AccountID,LastName,FirstName,MiddleName,Gender,Address,Birthdate,Specialization,ProfessionalFee,Email) VALUES ('$accountid','$accountid','','','','','','','','','$email')";
+    mysqli_query($con,$query2);  
+}
+
 $query= "INSERT into user_account(AccountID,AccessType,Passwordd,Email) VALUES ('$accountid','$accesstype','$password','$email')";
 
 mysqli_query($con,$query);  
