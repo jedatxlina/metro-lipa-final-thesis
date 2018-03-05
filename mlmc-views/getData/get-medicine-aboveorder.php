99c4d4354730a258d96c5a85248d165faf38c476
francisvgi <francisvonnilagan@gmail.com>
2018-03-05 18:30:19 +0800

    Pharmacy Department - Prioritized  Medicines that are under Re-Order Point

---
+++ mlmc-views/getData/get-medicine-aboveorder.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'connection.php';
+
+$sel = mysqli_query($con,"SELECT * FROM pharmaceuticals WHERE Quantity > ReOrder");
+
+$data = array();
+
+while ($row = mysqli_fetch_array($sel)) {
+    $data[] = array(
+        "PharmaID"=>$row['MedicineID'],
+        "PharmaName"=>$row['MedicineName'],        
+        "Unit"=>$row['Unit'],
+        "Price"=>$row['Price'],
+        "Quantity"=>$row['Quantity'],
+        "ReOrder"=>$row['ReOrder']);
+}
+echo json_encode($data);
+?>
+
+									
