10c3cabd9bb2750abf4cad359c0d1cde1918a6a2
francisvgi <francisvonnilagan@gmail.com>
2018-03-07 04:28:18 +0800

    Added Laboratory Department Module

---
+++ mlmc-views/updateData/update-laboratory-request.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'connection.php';
+
+
+$requestid = $_GET['requestid'];
+
+$query= "UPDATE laboratory_req SET Status = 'Cleared' WHERE RequestID = '$requestid'";
+
+mysqli_query($con,$query);  
+?>
