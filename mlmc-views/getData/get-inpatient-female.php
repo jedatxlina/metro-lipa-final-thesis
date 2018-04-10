9bbc74662e3f93bfa1d8f48dfeab8e33f31b2609
Francis Vonn Ilagan <francisvonnilagan@gmail.com>
2018-04-11 01:33:07 +0800

    Filtered the Female inpatients in Mother dropdown
    Nursery Tab from Patients Module

---
+++ mlmc-views/getData/get-inpatient-female.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'connection.php';
+
+$sel = mysqli_query($conn,"select * from patients where AdmissionType = 'Inpatient' AND Gender = 'Female'");
+
+$data = array();
+
+while ($row = mysqli_fetch_array($sel)) {
+    $data[] = array(
+    	"AdmissionID"=>$row['AdmissionID'],
+    	"AdmissionNo"=>$row['AdmissionNo'],
+		"AdmissionDate"=>$row['AdmissionDate'],
+		"AdmissionTime"=>$row['AdmissionTime'],
+    	"Fname"=>$row['FirstName'],
+    	"Mname"=>$row['MiddleName'],
+		"Lname"=>$row['LastName'],
+		"Admission"=>$row['Admission'],
+		"AdmissionType"=>$row['AdmissionType'],
+    	"Gender"=>$row['Gender'],
+        "Province"=>$row['Province'],
+        "City"=>$row['City'],
+        "CivilStatus"=>$row['CivilStatus'],
+        "Birthdate"=>$row['Birthdate'],
+        "Contact"=>$row['Contact']);
+}
+
+echo json_encode($data);
+?>
+
+									
