753377e8273f851e5e1bd9e9193bd53c2e5a8b6e
Francis Vonn Ilagan <francisvonnilagan@gmail.com>
2018-04-22 01:46:58 +0800

    Added Percentage Room Type Multiplier to Laboratory Bill

--- mlmc-views/insertData/insert-laboratory-bill.php
+++ mlmc-views/insertData/insert-laboratory-bill.php
@@ -17,6 +17,21 @@ while($row = mysqli_fetch_assoc($sel))
     $rate = $row['Rate'];
 }
 
+$sel2 = mysqli_query($conn,"SELECT beds.Percent
+FROM beds 
+JOIN patients
+JOIN medical_details
+WHERE patients.MedicalID = medical_details.MedicalID AND medical_details.BedID = beds.BedID AND patients.AdmissionID = 2017340646");
+
+$percent = '';
+
+
+while($row = mysqli_fetch_assoc($sel2))
+{
+    $percent = $row['Percent'];
+}
+$rate = $rate * $percent;
+
 $query = "INSERT into billing(BillID,AdmissionID,Department,ItemID,BillDes,TotalBill) 
 VALUES('$billid','$admissionid','$department','$laboratoryid','$des','$rate')";
 mysqli_query($conn,$query);  
