ad4c9c4102c49e414ce5889d481c7abf9e58d553
jedatxlina <jmatthewatx.lina@gmail.com>
2018-04-23 22:22:01 +0800

    Changes

--- mlmc-views/insertData/insert-laboratory-bill.php
+++ mlmc-views/insertData/insert-laboratory-bill.php
@@ -24,15 +24,7 @@ $sel2 = mysqli_query($conn,"SELECT beds.Percent
 FROM beds 
 JOIN patients
 JOIN medical_details
-<<<<<<< HEAD
-WHERE patients.MedicalID = medical_details.MedicalID AND medical_details.BedID = beds.BedID AND patients.AdmissionID = 2017340646");
-
-$percent = '';
-
-
-=======
 WHERE patients.MedicalID = medical_details.MedicalID AND medical_details.BedID = beds.BedID AND patients.AdmissionID = '$admissionid'");
->>>>>>> master
 while($row = mysqli_fetch_assoc($sel2))
 {
     $percent = $row['Percent'];
