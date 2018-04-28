2f8479eb5ee0501bf8ed9ccbc5464c26e990374b
Renz C. Marinez <marinez.renzc@gmail.com>
2018-04-28 17:36:21 +0800

    Revert "Minor Fixes"
    This reverts commit a56e79f7dc3e3deaaec9e3f842639c8562f1178e.

--- mlmc-views/insertData/insert-data-billing.php
+++ mlmc-views/insertData/insert-data-billing.php
@@ -5,10 +5,18 @@ require_once 'connection.php';
 $billid =  rand (11111,99999);
 $admissionid =  $_GET['id'];  
 $total =  $_GET['total'];
-
+$medid;
+$bedid;
 $item = '123123123';
-
-$query = "INSERT into billing(BillID,AdmissionID,Department,ItemID,BillDes,TotalBill, Status) 
-VALUES('$billid','$admissionid','Inpatient','1000','Inpatient Bill','$total','Paid')";
+$sel = mysqli_query($conn,"SELECT a.MedicalID, b. BedID, a.AdmissionType FROM patients a, medical_details b WHERE a.AdmissionID = '$admissionid' AND a.MedicalID = b.MedicalID");
+$data = array();
+while ($row = mysqli_fetch_array($sel)) {
+    $medid = $row['MedicalID'];
+    $bedid = $row['BedID'];
+    $desc = $row['AdmissionType'];
+}
+$description = $desc.' Bill';
+$query = "INSERT into billing(BillID,AdmissionID,Department,ItemID,BillDes,TotalBill, Status, MedicalID, BedID) 
+VALUES('$billid','$admissionid','Inpatient','1000','$description','$total','Paid','$medid','$bedid')";
 mysqli_query($conn,$query);  
 ?>
--- mlmc-views/insertData/insert-roomstart-date.php
+++ mlmc-views/insertData/insert-roomstart-date.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Singapore');
 $id = $_GET["id"];
 $bed;
 $admissno;
-$sel = mysqli_query($conn,"SELECT a.AdmissionNo, b.BedID FROM patients a, medical_details b WHERE a.AdmissionType = 'Inpatient' AND b.AdmissionID = '$id' AND a.AdmissionID = '$id'");
+$sel = mysqli_query($conn,"SELECT a.AdmissionNo, b.BedID FROM patients a, medical_details b WHERE a.AdmissionType = 'Inpatient' AND b.AdmissionID = '$id' AND a.AdmissionID = '$id' AND a.MedicalID = b.MedicalID");
 $data = array();
 while ($row = mysqli_fetch_array($sel)) {
     $bed = $row['BedID'];
@@ -13,6 +13,6 @@ while ($row = mysqli_fetch_array($sel)) {
 $billid2 =  rand (11111,99999);
 $date = date("Y-m-d h:i:sa");
 $query5 = "INSERT into duration(DurationID,AdmissionID,AdmissionNo,ArrivalDate,DischargeDate,BedID,TotalBill) 
-VALUES('$billid2','$id','$admissno','$date','0000-00-00','$bedno','0000')";
+VALUES('$billid2','$id','$admissno','$date','0000-00-00 00:00:00','$bedno','0000')";
 mysqli_query($conn,$query5);
 ?>
