9056419bac9e53e091c62b7629ac8ac98b5fe92e
Francis Vonn Ilagan <francisvonnilagan@gmail.com>
2018-04-28 22:16:31 +0800

    Added laboratory result when clearing in Laboratory Department

--- populatedData/laboratory_req.sql
+++ populatedData/laboratory_req.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2018 at 03:34 PM
+-- Generation Time: Apr 28, 2018 at 04:10 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -38,16 +38,23 @@ CREATE TABLE `laboratory_req` (
   `TimeRequest` varchar(25) NOT NULL,
   `DateCleared` varchar(25) NOT NULL,
   `TimeCleared` varchar(25) NOT NULL,
-  `MedicalID` int(15) NOT NULL
+  `MedicalID` int(15) NOT NULL,
+  `Result` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `laboratory_req`
 --
 
-INSERT INTO `laboratory_req` (`RequestID`, `LaboratoryID`, `AdmissionID`, `Status`, `SpecialRequest`, `DateRequest`, `TimeRequest`, `DateCleared`, `TimeCleared`, `MedicalID`) VALUES
-(118890, 1008, 2017340646, 'Cleared', '', '2018-04-22', '01:44 AM', '', '', 0),
-(735728, 1008, 2017340646, 'Cleared', '', '2018-04-22', '01:18 AM', '', '', 0);
+INSERT INTO `laboratory_req` (`RequestID`, `LaboratoryID`, `AdmissionID`, `Status`, `SpecialRequest`, `DateRequest`, `TimeRequest`, `DateCleared`, `TimeCleared`, `MedicalID`, `Result`) VALUES
+(118890, 1008, 2017340646, 'Cleared', '', '2018-04-22', '01:44 AM', '', '', 0, ''),
+(293279, 302776, 2017340646, 'Cleared', '', '2018-04-23', '11:40 PM', '2018-04-23', '11:41 PM', 533909, ''),
+(394804, 123723, 2017319597, 'Cleared', '', '2018-04-23', '10:24 PM', '2018-04-23', '10:24 PM', 198059, ''),
+(453301, 1087, 2017319597, 'Cleared', '', '2018-04-23', '10:19 PM', '2018-04-23', '10:19 PM', 198059, ''),
+(587648, 1008, 2017319597, 'Cleared', '', '2018-04-23', '09:50 PM', '2018-04-23', '09:50 PM', 198059, ''),
+(687715, 1075, 2017319597, 'Cleared', '', '2018-04-23', '09:55 PM', '2018-04-23', '09:55 PM', 198059, ''),
+(735728, 1008, 2017340646, 'Cleared', '', '2018-04-22', '01:18 AM', '', '', 0, ''),
+(798824, 1008, 2017340646, 'Cleared', '', '2018-04-25', '10:12 PM', '2018-04-28', '10:08 PM', 168164, '');
 
 --
 -- Indexes for dumped tables
