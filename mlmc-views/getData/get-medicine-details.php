4fd1c16408d88ff56b18cd1275e14979d91ad2a2
Francis Vonn Ilagan <francisvonnilagan@gmail.com>
2018-05-03 03:05:18 +0800

    Removed Medicines that has 0 price in Multi selector of medicines

--- mlmc-views/getData/get-medicine-details.php
+++ mlmc-views/getData/get-medicine-details.php
@@ -1,13 +1,14 @@
 <?php
 require_once 'connection.php';
 
-$sel = mysqli_query($conn,"SELECT MedicineID, CONCAT(MedicineName, ' ' ,Unit) AS MedicineName FROM pharmaceuticals");
+$sel = mysqli_query($conn,"SELECT MedicineID, CONCAT(MedicineName, ' ' ,Unit) AS MedicineName , Price FROM pharmaceuticals WHERE Price > 0");
 
 $data = array();
 
 while ($row = mysqli_fetch_array($sel)) {
     $data[] = array(
 		"MedicineID"=>$row['MedicineID'],
+		"Price"=>$row['Price'],
 		"MedicineName"=>$row['MedicineName']);
 }
 echo json_encode($data);
--- mlmc-views/getData/get-pharmaceutical-details.php
+++ mlmc-views/getData/get-pharmaceutical-details.php
@@ -2,7 +2,7 @@
 
 require_once 'connection.php';
 
-$sel = mysqli_query($conn,"SELECT * FROM pharmaceuticals");
+$sel = mysqli_query($conn,"SELECT * FROM pharmaceuticals WHERE Price > 0");
 
 $data = array();
 
