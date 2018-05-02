3a0cf9b23b0324c4cdc2225fdc522b19389fc841
Francis Vonn Ilagan <francisvonnilagan@gmail.com>
2018-05-02 21:36:05 +0800

    Fixed Referral

--- mlmc-views/getData/get-referral-details.php
+++ mlmc-views/getData/get-referral-details.php
@@ -4,13 +4,14 @@ require_once 'connection.php';
 
 $at = $_GET['at'];
 
-$sel = mysqli_query($conn,"SELECT * FROM referrals JOIN physicians WHERE ReferredTo = '$at' AND physicians.PhysicianID = referrals.ReferredBy");
+$sel = mysqli_query($conn,"SELECT physicians.*,CONCAT (patients.FirstName, ' ',patients.MiddleName, ' ',patients.LastName) AS PatientName FROM referrals JOIN physicians,patients WHERE ReferredTo = '$at' AND physicians.PhysicianID = referrals.ReferredBy AND patients.AdmissionID = referrals.AdmissionID");
 
 $data = array();
 
 while ($row = mysqli_fetch_array($sel)) {
     $data[] = array(
         "ID"=>$row['ID'],
+        "PatientName"=>$row['PatientName'],
         "AdmissionID"=>$row['AdmissionID'],
         "ReferredTo"=>$row['ReferredTo'],
         "ReferredBy"=>$row['ReferredBy'],
